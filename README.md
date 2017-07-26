60c585006bc480aa3745a8281c386ce71c38eab4
tuz358 <orca00358@gmail.com>
2017-07-26 22:28:23 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # matrix-lib
+
+Matrix library implemented in various languages.
+
+### Java
+---
+OS: macOS 10.12.6
+Version: javac 1.8.0_121
+
+** Tutorial **
+> $ git clone http://github.com/tuz358/matrix-lib
+> $ cd ./matrix-lib-master/java/
+> $ javac Test.java matrix.java
+> $ java Test
+
+
+### Python
+---
+OS: macOS 10.12.6
+Version: Python2.7.13, 3.6.0
+
+** Getting Started **
+> $ git clone http://github.com/tuz358/matrix-lib
+> $ cd ./matrix-lib-master/python/
+
+** Example **
+> $ python
+> >>> import matrix
+> >>> a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+> >>> b = matrix.identity(3)
+> >>> b
+> [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+> >>> matrix.dot(a, b)
+> [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+> >>>
--- java/Test.java
+++ java/Test.java
@@ -9,7 +9,7 @@ class Test {
     matrix B = new matrix(b);
     matrix C = new matrix(3, 3);
 
-    System.out.println("C = dot(A, B)");
+    System.out.println("C = A.dot(B)");
     System.out.println("------------------");
     C = A.dot(B);
     printmatrix(A, "A");
@@ -17,37 +17,37 @@ class Test {
     printmatrix(C, "C");
     System.out.println("------------------");
 
-    System.out.println("A_t = transpose(A)");
+    System.out.println("A_t = A.transpose()");
     System.out.println("------------------");
     matrix A_t = A.transpose();
     printmatrix(A_t, "A_t");
     System.out.println("------------------");
 
-    System.out.println("C = outer(A, B)");
+    System.out.println("C = A.outer(B)");
     System.out.println("------------------");
     C = A.outer(B);
     printmatrix(C, "C");
     System.out.println("------------------");
 
-    System.out.println("tr_A = trace(A)");
+    System.out.println("tr_A = A.trace()");
     System.out.println("------------------");
     float tr_A = A.trace();
     System.out.println(java.text.MessageFormat.format("tr_A: {0}\n", tr_A));
     System.out.println("------------------");
 
-    System.out.println("E3 = identity(3)");
+    System.out.println("E3 = new matrix(3, 3).identity(3)");
     System.out.println("------------------");
     matrix E3 = new matrix(3, 3).identity(3);
     printmatrix(E3, "E3");
     System.out.println("------------------");
 
-    System.out.println("C = multiply(A, B)");
+    System.out.println("C = A.multiply(B)");
     System.out.println("------------------");
     C = A.multiply(B);
     printmatrix(C, "C");
     System.out.println("------------------");
 
-    System.out.println("C = direct_sum(A, B)");
+    System.out.println("C = A.direct_sum(B)");
     System.out.println("------------------");
     C = A.direct_sum(B);
     printmatrix(C, "C");
