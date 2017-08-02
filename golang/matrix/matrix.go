6c7162f9a4bf4ee92d90f6ba25477b1ec803916f
tuz358 <orca00358@gmail.com>
2017-08-03 05:03:30 +0900

    Created golang version

---
+++ golang/matrix/matrix.go
@@ -0,0 +1,103 @@
+package matrix
+
+func Dot(a [][]float32, b [][]float32) ([][]float32) {
+	out := make([][]float32, len(b[0]))
+	for i := range out {
+		out[i] = make([]float32, len(a))
+	}
+
+	for j := 0 ; j < len(b) ; j++ {
+		for i := 0 ; i < len(a) ; i++ {
+			for k := 0 ; k < len(a[0]) ; k++ {
+				out[i][j] += a[i][k]*b[k][j]
+			}
+		}
+	}
+	return out
+}
+
+func Transpose(a [][]float32) ([][]float32) {
+	transposed := make([][]float32, len(a[0]))
+	for i := range transposed {
+		transposed[i] = make([]float32, len(a))
+	}
+
+	for j := 0; j < len(a[0]); j++ {
+		for i := 0; i < len(a); i++ {
+			transposed[i][j] = a[j][i]
+		}
+	}
+	return transposed
+}
+
+func Outer(a [][]float32, b [][]float32) ([][]float32) {
+	out := make([][]float32, len(b)*len(b[0]))
+	for i := range out {
+		out[i] = make([]float32, len(a)*len(a[0]))
+	}
+
+	a_flatten := make([]float32, len(a)*len(a[0]))
+	b_flatten := make([]float32, len(b)*len(b[0]))
+
+	for i := 0; i < len(a_flatten); i++ {
+		a_flatten[i] = a[int(i/len(a))][int(i%len(a[0]))]
+	}
+	for j := 0; j < len(b_flatten); j++ {
+		b_flatten[j] = b[int(j/len(b))][int(j%len(b[0]))]
+	}
+
+	for j := 0; j < len(b_flatten); j++ {
+		for i := 0; i < len(a_flatten); i++ {
+			out[i][j] = a_flatten[i] * b_flatten[j]
+		}
+	}
+	return out
+}
+
+func Trace(a [][]float32) (float32) {
+	var trace_a float32 = 0
+	for i := 0; i < len(a); i++ {
+		trace_a += a[i][i]
+	}
+	return trace_a
+}
+
+func Identity(n int) ([][]float32) {
+	E := make([][]float32, n)
+	for i := range E {
+		E[i] = make([]float32, n)
+	}
+
+	for i := 0; i < n; i++ {
+		E[i][i] = 1
+	}
+	return E
+}
+
+func Multiply(a [][]float32, b [][]float32) ([][]float32) {
+	out := make([][]float32, len(a[0]))
+	for i := range out {
+		out[i] = make([]float32, len(a))
+	}
+
+	for j := 0; j < len(a[0]); j++ {
+		for i := 0; i < len(a); i++ {
+			out[i][j] = a[i][j] * b[i][j]
+		}
+	}
+	return out
+}
+
+func Direct_sum(a [][]float32, b [][]float32) ([][]float32) {
+	out := make([][]float32, len(a[0]))
+	for i := range out {
+		out[i] = make([]float32, len(a))
+	}
+
+	for j := 0; j < len(a[0]); j++ {
+		for i := 0; i < len(a); i++ {
+			out[i][j] = a[i][j] + b[i][j]
+		}
+	}
+	return out
+}
---
+++ golang/sample.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+  "fmt"
+  "./matrix"
+)
+
+func main () {
+  a := [][]float32{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+  b := [][]float32{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+
+  fmt.Println("matrix a:")
+  printmatrix(a)
+  fmt.Println()
+
+  fmt.Println("matrix b:")
+  printmatrix(b)
+  fmt.Println()
+
+  fmt.Println("Dot(a, b):")
+  printmatrix(matrix.Dot(a, b))
+  fmt.Println()
+
+  fmt.Println("Transpose(a):")
+  printmatrix(matrix.Transpose(a))
+  fmt.Println()
+
+  fmt.Println("Outer(a, b):")
+  printmatrix(matrix.Outer(a, b))
+  fmt.Println()
+
+  fmt.Println("Trace(a):")
+  fmt.Println(matrix.Trace(a))
+  fmt.Println()
+
+  fmt.Println("Identity(3):")
+  printmatrix(matrix.Identity(3))
+  fmt.Println()
+
+  fmt.Println("Multiply(a, b):")
+  printmatrix(matrix.Multiply(a, b))
+  fmt.Println()
+
+  fmt.Println("Direct_sum(a, b):")
+  printmatrix(matrix.Direct_sum(a, b))
+  fmt.Println()
+}
+
+func printmatrix(mat [][]float32){
+  for i := 0; i < len(mat); i++ {
+    for j := 0; j < len(mat[0]); j++ {
+      fmt.Printf("%v ", mat[i][j])
+    }
+    fmt.Println()
+  }
+  return
+}
